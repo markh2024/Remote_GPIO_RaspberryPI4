aac7a8f6f0a7ce2b8f800c22ca72bc493734881f
markh2024 <159157948+markh2024@users.noreply.github.com>
2025-09-01 00:03:10 +0100

    Change to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main() {
     // Replace with the IP of your Raspberry Pi running pigpiod
-    const char* remotePi = "192.168.1.50";
-
+    const char* remotePi = "192.168.0.167";
+    int count = 0 ;
     int pi = pigpio_start(remotePi, NULL); // connect to pigpiod daemon
     if (pi < 0) {
         std::cerr << "❌ Failed to connect to pigpiod at " << remotePi << std::endl;
@@ -18,9 +18,16 @@ int main() {
     int gpio = 17;
     set_mode(pi, gpio, PI_OUTPUT);
 
+    for (count= 0 ; count <10; count ++){
     std::cout << "🔆 Turning GPIO " << gpio << " ON" << std::endl;
     gpio_write(pi, gpio, 1);
-    sleep(1);
+    sleep(5);
+    std::cout << "🌑 Turning GPIO " << gpio << " OFF" << std::endl;
+    gpio_write(pi, gpio, 0);
+
+    sleep(5);
+
+    }
 
     std::cout << "🌑 Turning GPIO " << gpio << " OFF" << std::endl;
     gpio_write(pi, gpio, 0);
